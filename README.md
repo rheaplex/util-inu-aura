36938f521354ad94060a12d6e4ea9cdbf7eeb606
Rhea Myers <rhea@myers.studio>
2024-10-25 17:26:44 -0700

    Connecting and minting etc. work again. Filled out README a little.

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ AURA: The aura of art - its presence, cultural cachet, or specialness.
 # Notes
 
 Uses code from https://github.com/tokenbound/contracts .
+
+# Development
+
+## Run Tests
+
+    forge test
+
+## Deploy Locally
+
+This is anvil's local account 0 key.
+
+    anvil
+    forge script script/Deploy.s.sol --broadcast --chain-id 31337 --rpc-url http://127.0.0.1:8545 \
+        --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+    yarn wagmi generate
+    python -m http.server
--- app/js/util-inu-aura.js
+++ app/js/util-inu-aura.js
@@ -1,15 +1,18 @@
 import {
+  http,
   createPublicClient,
+  createWalletClient,
   custom,
   erc20Abi,
   erc721Abi,
-} from 'viem';
-import { mainnet } from 'viem/chains';
+  getContract
+} from 'https://esm.sh/viem';
+import { anvil, mainnet } from 'https://esm.sh/viem/chains';
 
 import {
   utilAddress, inuAddress, auraAddress,
   utilAbi, inuAbi, auraAbi
-} from './generated';
+} from './generated.js';
 
 ////////////////////////////////////////////////////////////////////////
 // Contract details.
@@ -31,17 +34,16 @@ const ABIS = {
 // State.
 ////////////////////////////////////////////////////////////////////////
 
+let walletClient;
+let publicClient;
+let walletAddress;
 let tokenAddress;
 let tokenId;
 let metadata;
 let imgUrl;
 let aura;
 let unwatchAuraCallback;
-
-const client = createPublicClient({
-  chain: mainnet,
-  transport: custom(window.ethereum),
-});
+let balanceIntervalId;
 
 ////////////////////////////////////////////////////////////////////////
 // Http/ipfs network access.
@@ -73,28 +75,19 @@ async function fetchMetadata () {
 // Blockchain interaction.
 ////////////////////////////////////////////////////////////////////////
 
-async function readBalances (symbols) {
-  const chainId = await client.getChainId();
-  const address = client.requestAddresses()[0];
-  const contracts = symbols.map(symbol => {
-    return ({
-      address: ADDRESSES[symbol][chainId],
-      abi: erc20Abi,
-      functionName: "balanceOf",
-      args: [ address ]
-    });
+async function readBalance (symbol) {
+  const chainId = await publicClient.getChainId();
+  return publicClient.readContract({
+    abi: erc20Abi,
+    address: ADDRESSES[symbol][chainId],
+    functionName: "balanceOf",
+    args: [ walletAddress ]
   });
-  const results = await client.multiCall({ contracts });
-  let i = 0;
-  return results.map(result => ({
-    symbol: symbols[i++],
-    value: result.status == "success" ? result.result : "error"
-  }));
 }
 
 async function readAura () {
-  const chainId = client.chainId();
-  aura = await client.readContract({
+  const chainId = await publicClient.getChainId();
+  aura = await publicClient.readContract({
     abi: auraAbi,
     address: auraAddress[chainId],
     functionName: 'auraOf',
@@ -109,10 +102,11 @@ async function unwatchAura () {
   }
 }
 
-export async function watchAura () {
+async function watchAura () {
   unwatchAura();
-  unwatchAuraCallback = client.watchContractEvent({
-    address: auraAddress,
+  const chainId = await publicClient.getChainId();
+  unwatchAuraCallback = publicClient.watchContractEvent({
+    address: auraAddress[chainId],
     abi: auraAbi,
     eventName: "Transfer",
     // FIXME:
@@ -121,39 +115,48 @@ export async function watchAura () {
   });
 }
 
-async function writeMint (symbol,address, amount) {
-  const chainId = client.chainId();
-  return client.writeContract({
+async function writeMint (symbol, amount) {
+  const chainId = await walletClient.getChainId();
+  return walletClient.writeContract({
     abi: ABIS[symbol],
     address: ADDRESSES[symbol][chainId],
     functionName: 'mint',
-    args: [ address, BigInt(amount) ],
+    args: [ walletAddress, BigInt(amount) ],
+    account: walletAddress
   });
 }
 
 async function writeApproveUtils (amount) {
-  const chainId = client.chainId();
-  return client.writeContract({
+  const chainId = await walletClient.getChainId();
+  return walletClient.writeContract({
     abi: utilAbi,
     address: utilAddress[chainId],
     functionName: 'approve',
     args: [inuAddress[chainId], BigInt(amount)],
+    account: walletAddress
   });
 }
 
 async function writeBurnUtilForInu (amount) {
-  const chainId = client.chainId();
-  return client.writeContract({
+  const chainId = await walletClient.getChainId();
+  return walletClient.writeContract({
     abi: inuAbi,
     address: inuAddress[chainId],
     functionName: 'burnUtilForInu',
     args: [BigInt(amount)],
+    account: walletAddress
   });
 }
 
 ////////////////////////////////////////////////////////////////////////
 // Gui manipulation.
 ////////////////////////////////////////////////////////////////////////
+
+function setWalletAddress (address) {
+  walletAddress = address || false;
+  [...document.getElementsByClassName("wallet-address")]
+    .forEach(element => element.innerHTML = walletAddress || "—");
+}
 
 function setGuiAura () {
   if (imgUrl && aura) {
@@ -174,11 +177,31 @@ async function leaveAuraDisplay () {
   return unwatchAura();
 }
 
+function show (element) {
+  document.getElementById(element).hidden = false;
+}
+
+function hide (element) {
+  document.getElementById(element).hidden = true;
+}
+
+function disableMintForms () {
+  Array.from(document.querySelectorAll(
+    "#address-mint input, #address-mint button"
+  )).forEach(element => element.disabled = true);
+}
+
+function enableMintForms () {
+  Array.from(document.querySelectorAll(
+    "#address-mint input, #address-mint button"
+  )).forEach(element => element.disabled = false);
+}
+
 ////////////////////////////////////////////////////////////////////////
 // Gui Event Handlers.
 ////////////////////////////////////////////////////////////////////////
 
-export async function handleCheckAura (e) {
+async function handleCheckAura (e) {
   const formData = new FormData(e.target);
   tokenAddress = formData.get('address');
   tokenId = formData.get('id');
@@ -190,22 +213,21 @@ export async function handleCheckAura (e) {
 
 async function handleMint (symbol, e) {
   e.preventDefault();
-  const address = client.requestAddresses()[0];
   const formData = new FormData(e.target);
   const amount = formData.get('amount');
   e.target.elements["amount"].value = "";
-  return writeMint(symbol, address, amount);
+  return writeMint(symbol, amount);
 }
 
-export async function handleMintUtils (e) {
+async function handleMintUtils (e) {
   return handleMint("UTIL", e);
 }
 
-export async function handleMintAura (e) {
+async function handleMintAura (e) {
   return handleMint("AURA", e);
 }
 
-export function handleApproveUtils (e) {
+function handleApproveUtils (e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const amount = formData.get('amount');
@@ -213,10 +235,74 @@ export function handleApproveUtils (e) {
   return writeApproveUtils(amount);
 }
 
-export async function handleBurnUtilForInu (e) {
+async function handleBurnUtilForInu (e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const amount = formData.get('amount');
   e.target.elements["amount"].value = "";
   return writeBurnUtilForInu(amount);
+}
+
+async function handleConnectAddress (e) {
+  e.preventDefault();
+  const [ address ] = await walletClient.requestAddresses();
+  if (address) {
+    setWalletAddress(address);
+    hide("connect-address");
+    show("disconnect-address");
+    enableMintForms();
+    await updateBalances();
+    balanceIntervalId = setInterval(updateBalances, 10000);
+  }
+  return false;
+}
+
+function handleDisconnectAddress (e) {
+  e.preventDefault();
+  setWalletAddress(false);
+  show("connect-address");
+  hide("disconnect-address");
+  disableMintForms();
+  clearInterval(balanceIntervalId);
+  balanceIntervalId = undefined;
+  document.getElementById("util-balance").innerHTML = "-";
+  document.getElementById("inu-balance").innerHTML = "-";
+  document.getElementById("aura-balance").innerHTML = "-";
+  return false;
+}
+
+function onSubmit (id, handler) {
+  document.getElementById(id)
+    .addEventListener("submit", handler);
+}
+
+async function updateBalances () {
+  if (walletAddress) {
+    document.getElementById("util-balance").innerHTML =
+      await readBalance("UTIL");
+    document.getElementById("inu-balance").innerHTML =
+      await readBalance("INU");
+    document.getElementById("aura-balance").innerHTML =
+      await readBalance("AURA");
+  }
+
+}
+
+export function init () {
+  const chain = anvil; //mainnet,
+  publicClient = createPublicClient({
+    chain,
+    transport: http(),
+  });
+  walletClient = createWalletClient({
+    chain,
+    transport: custom(window.ethereum),
+  });
+  onSubmit("connect-address", handleConnectAddress);
+  onSubmit("disconnect-address", handleDisconnectAddress);
+  onSubmit("mint-util", handleMintUtils);
+  onSubmit("mint-aura", handleMintAura);
+  onSubmit("approve-utils", handleApproveUtils);
+  onSubmit("burn-utils", handleBurnUtilForInu);
+  onSubmit("check-aura", handleCheckAura);
 }

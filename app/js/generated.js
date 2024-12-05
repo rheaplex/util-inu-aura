d49ee19cba3d7b4668bcf99cfc9ee11ebc74a958
Rhea Myers <rhea@myers.studio>
2024-12-04 17:13:33 -0800

    Ready to deploy.

--- app/js/generated.js
+++ app/js/generated.js
@@ -322,7 +322,7 @@ export const auraAbi = [
  *
  */
 export const auraAddress = {
-  31337: '0x68B1D87F95878fE05B998F19b66F4baba5De1aed',
+  31337: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
 }
 
 /**
@@ -616,7 +616,7 @@ export const inuAbi = [
  *
  */
 export const inuAddress = {
-  31337: '0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE',
+  31337: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
 }
 
 /**
@@ -925,7 +925,7 @@ export const utilAbi = [
  *
  */
 export const utilAddress = {
-  31337: '0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1',
+  31337: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
 }
 
 /**
